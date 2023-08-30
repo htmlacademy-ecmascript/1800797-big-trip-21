59ec1ec4a37b78b9b353b582bb557ab59f1c2ebf
Kirill <89221688844@mail.ru>
2023-08-30 14:51:46 +0500

    Выполняет компоненты

---
+++ src/main.js
@@ -0,0 +1,8 @@
+import HeaderPresenter from './presenter/header-presenter.js';
+import BoardPresenter from './presenter/board-presenter.js';
+
+const headerPresenter = new HeaderPresenter();
+headerPresenter.init();
+
+const boardPresenter = new BoardPresenter();
+boardPresenter.init();
